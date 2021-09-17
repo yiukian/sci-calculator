4e62883db7011d998d4175fe859bba4e1c10b236
kianyiu <kianyiu20@gmail.com>
2021-09-17 21:28:22 +0800

    - Change App page for basic showing

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,50 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { APP_TITLE } from './config.js';
+import { Row, Col } from 'react-bootstrap';
+import Container from 'react-bootstrap/Container'
 
+/*
+function App() {
+  return (
+    <Container>
+  <Row className="justify-content-md-center">
+    <Col xs lg="2">
+      1 of 3
+    </Col>
+    <Col md="auto">Variable width content</Col>
+    <Col xs lg="2">
+      3 of 3
+    </Col>
+  </Row>
+  <Row>
+    <Col>a of c</Col>
+    <Col md="auto">Variable width content</Col>
+    <Col xs lg="2">
+      c of c
+    </Col>
+  </Row>
+</Container>
+
+  )
+}
+*/
 
 function App() {
   return (
+      <Container fluid>
+        <Row>
+          <Col className="App-header"><img src={logo} className="App-logo" alt="logo" />{APP_TITLE}</Col>
+        </Row>
+        <Row>
+          <Col/>
+        </Row>
+        <Row><Col xs={2} /><Col className="Lcd-body">0123456789.00</Col><Col xs={2} /></Row>
+      </Container>
+  );
+}
+
+/*
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -21,17 +61,17 @@ function App() {
             </tr>
             <tr className="Keypad">
               <td>
-                {/* keypad 1 */ }
+                {/* keypad 1  }
               </td>
               <td>
-                {/* keypad 2 */ }
+                {/* keypad 2  }
               </td>
             </tr>
           </tbody>
         </table>
       </div>
     </div>
-  );
-}
+  </div>
+*/
 
 export default App;
