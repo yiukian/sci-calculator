08767cc3ad0036f7b5173346a96a6afd69b3b489
kianyiu <kianyiu20@gmail.com>
2021-10-13 10:14:59 +0800

    - Fix LCD display in LCD container using connector

--- src/containers/connector/LcdPanel.tsx
+++ src/containers/connector/LcdPanel.tsx
@@ -1,10 +1,14 @@
 import React from "react";
-import { connect, ConnectedProps, useSelector } from "react-redux";
-import { AppState, State } from "../../redux";
+import { connect, ConnectedProps } from "react-redux";
+import { State } from "../../redux";
 
 interface Props extends PropsFromRedux {}
 
-const mapStateToProps = (state: State) => ({});
+const mapStateToProps = (state: State) => ({
+  operand1: state.keys.operand1,
+  operand2: state.keys.operand2,
+  whatToShow: state.keys.whatToShow,
+});
 
 const mapDispatchToProps = {};
 
@@ -13,11 +17,15 @@ const connector = connect(mapStateToProps, mapDispatchToProps);
 type PropsFromRedux = ConnectedProps<typeof connector>;
 
 const LcdPanel: React.FC<Props> = (props: Props) => {
-  const operand1: string = useSelector(
-    (state: AppState) => state.keys.operand1
-  );
+  const { operand1, operand2, whatToShow } = props;
+  var lcdDisplay: string;
 
-  return <div className="LcdBody">{operand1}</div>;
+  if (whatToShow === "operand2") {
+    lcdDisplay = operand2;
+  } else {
+    lcdDisplay = operand1;
+  }
+  return <div className="LcdBody">{lcdDisplay}</div>;
 };
 
 export default connector(LcdPanel);
